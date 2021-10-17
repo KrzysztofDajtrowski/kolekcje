c7d105bf7af45bcd20c2218a81d08bc3a3cb3b58
KrzysztofDajtrowski <krzysztofdajtrowski@gmail.com>
2021-10-17 15:18:51 +0200

    pierwszy commit

---
+++ src/pl/camp/it/kolekcje/listy/App.java
@@ -0,0 +1,18 @@
+package pl.camp.it.kolekcje.listy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+
+        
+        System.out.println(list);
+    }
+}
